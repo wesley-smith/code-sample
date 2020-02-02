23fd2999e396415ed8ac5b2e8eafbae5446b950c
Wesley Smith <wcsmith.atl@gmail.com>
2020-02-02 08:29:31 -0500

    Categorize by power generation type

--- power.py
+++ power.py
@@ -1,4 +1,24 @@
+from enum import Enum
+
+
+class PowerCategories(Enum):
+    """
+    Major power categories according to the US EIA:
+
+    https://www.eia.gov/energyexplained/electricity/electricity-in-the-us.php
+    """
+
+    FOSSIL = "FOSSIL"
+    NUCLEAR = "NUCLEAR"
+    RENEWABLE = "RENEWABLE"
+
+
 class PowerPlant:
-    def __init__(self, capacity, fuel_type):
+    def __init__(self, capacity: int, category: str):
+        sanitized_category = category.upper()
+        if not PowerCategories.__members__.__contains__(sanitized_category):
+            raise ValueError(
+                f'Expected "{category}" to be one of {", ".join(item[0] for item in PowerCategories.__members__.items())}'
+            )
         self.capacity = capacity
-        self.fuel_type = fuel_type
+        self.category = sanitized_category
--- test/test_power.py
+++ test/test_power.py
@@ -1,13 +1,26 @@
 from unittest import TestCase
 
-from power import PowerPlant
+from power import PowerPlant, PowerCategories
 
 
 class PowerPlantTest(TestCase):
     def test_constructor(self):
         capacity = 10000
-        fuel_type = 'fossil'
-        plant = PowerPlant(capacity, fuel_type)
+        category = PowerCategories.FOSSIL.value
 
-        self.assertEqual(plant.capacity, capacity)
-        self.assertEqual(plant.fuel_type, fuel_type)
+        with self.subTest("power categories are case-insensitive"):
+            plant_1 = PowerPlant(capacity, "FOSSIL")
+            plant_2 = PowerPlant(capacity, "fossil")
+            self.assertEqual(plant_1.capacity, capacity)
+            self.assertEqual(plant_1.category, category)
+            self.assertEqual(plant_2.capacity, capacity)
+            self.assertEqual(plant_2.category, category)
+
+        with self.subTest("invalid categories produce an error"):
+            with self.assertRaises(ValueError) as handler:
+                PowerPlant(capacity, "foo")
+
+            self.assertEqual(
+                handler.exception.args[0],
+                'Expected "foo" to be one of FOSSIL, NUCLEAR, RENEWABLE',
+            )
